b6a6037a3eeeb52f5a9aa40a723755dc012de005
David711 <36839385+David711@users.noreply.github.com>
2018-02-26 11:49:37 +0100

    comentario

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title> Blog </title>
+     <link href="estilos.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div class="cabecera">
+    <div class="contenidocabecera">
+        
+    </div>
+
+</div>
+    
+<div class="fondo">
+    <div class="contenido">
+        
+    </div>
+ 
+</div>
+<div class="pie">
+    <div class="contenidopie">
+        
+    </div>
+    
+</div>
+    
+</body>
+</html>
+
+
+
