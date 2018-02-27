96416afd30bb637a217b382d9e7c661b7cb37cf3
David711 <36839385+David711@users.noreply.github.com>
2018-02-27 13:12:45 +0100

    index sin php

--- index.php
+++ index.php
@@ -17,6 +17,30 @@
     
 <div class="fondo">
     <div class="contenido">
+        <div class="entrada">
+            
+           <div class="contenedorimagen">
+             <img class="imagen" src="imagenes/comunes/junta.jpg" alt="junta" title="junta"/>
+            
+           </div> 
+           <div class="informacion">
+            <p class="fecha"> 27 de Febrero de 2018</p>
+            <h2 class="titulo"> EJEMPLO DE TITULO DE ENTRADA </h2>
+            <p class="texto"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+            vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+            </p>
+            
+            <form action="entrada.php" method="post" enctype="multipart/form-data">
+                
+               <input type="submit" class="boton leer" value="LEER MAS"/> 
+                    
+            </form>
+            
+           </div>
+            
+        </div>
         
     </div>
  
